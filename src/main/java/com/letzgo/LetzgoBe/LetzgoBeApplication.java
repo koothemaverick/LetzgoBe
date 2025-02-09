462a8b561303e26711c976bc9397f821f9968409
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-02-09 17:54:24 +0900

    Initial commit

---
+++ src/main/java/com/letzgo/LetzgoBe/LetzgoBeApplication.java
@@ -0,0 +1,13 @@
+package com.letzgo.LetzgoBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetzgoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetzgoBeApplication.class, args);
+	}
+
+}
