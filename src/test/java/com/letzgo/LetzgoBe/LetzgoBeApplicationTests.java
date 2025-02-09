21952ec6fe2bb5ba640b10f6357ffaa0e8cfca06
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-02-09 17:54:24 +0900

    Initial commit

---
+++ src/test/java/com/letzgo/LetzgoBe/LetzgoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letzgo.LetzgoBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetzgoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
