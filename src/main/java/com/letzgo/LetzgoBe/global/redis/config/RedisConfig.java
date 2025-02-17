a929b3c840929c1cdb296a69344dc3c3025fd1bd
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-02-17 22:37:38 +0900

    RedisConfig수정
    stringRedisTemplate -> redisTemplate 타입 변경

--- src/main/java/com/letzgo/LetzgoBe/global/redis/config/RedisConfig.java
+++ src/main/java/com/letzgo/LetzgoBe/global/redis/config/RedisConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
@@ -17,7 +19,11 @@ public class RedisConfig {
     }
 
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
-        return new StringRedisTemplate(connectionFactory);
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+         template.setKeySerializer(new StringRedisSerializer());
+         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
     }
 }
