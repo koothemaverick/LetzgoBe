3379f75ea3a8d21fbb856513b812a9db5308b4c5
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-02-18 21:21:57 +0900

    .

--- src/main/java/com/letzgo/LetzgoBe/domain/account/user/repository/UserRepository.java
+++ src/main/java/com/letzgo/LetzgoBe/domain/account/user/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.letzgo.LetzgoBe.domain.account.user.repository;
 import com.letzgo.LetzgoBe.domain.account.user.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
