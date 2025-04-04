581e34444054ac1d273a371d8fe078e7c2a6b692
uichan01 <uichan02@naver.com>
2025-04-05 00:01:02 +0900

    config수정
    주석처리

--- src/main/java/com/letzgo/LetzgoBe/domain/dataFetcher/config/DriverConfig.java
+++ src/main/java/com/letzgo/LetzgoBe/domain/dataFetcher/config/DriverConfig.java
@@ -20,7 +20,7 @@ public class DriverConfig {
         options.addArguments("--start-maximized");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
-        //options.addArguments("--headless"); //주석처리시 브라우저 보임
+        options.addArguments("--headless"); //주석처리시 브라우저 보임
 
         return new ChromeDriver(options);
     }
