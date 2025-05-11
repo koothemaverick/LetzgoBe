7ac418939d59e1118621da284a99cdc8dc62e8e6
Pyeonggeun <rjal3054@gmail.com>
2025-05-12 00:08:49 +0900

    schedule 수정

--- src/main/java/com/letzgo/LetzgoBe/domain/schedule/repository/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.letzgo.LetzgoBe.domain.schedule.repository;
-
-import com.letzgo.LetzgoBe.domain.schedule.entity.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
