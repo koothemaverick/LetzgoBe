056787bba3d908b2bb5dd0cf74b4a8f8871fabd9
uichan01 <uichan02@naver.com>
2025-05-11 18:38:56 +0900

    호텔,식당 지역명 통일

--- src/main/java/com/letzgo/LetzgoBe/domain/dataFetcher/service/InfoProvideService.java
+++ src/main/java/com/letzgo/LetzgoBe/domain/dataFetcher/service/InfoProvideService.java
@@ -21,9 +21,10 @@ public class InfoProvideService {
     private final HotelRepository hotelRepository;
     private final RestaurantRepository restaurantRepository;
 
+    String[] regions = {"경기도", "제주특별자치도", "충청남도", "인천광역시", "대구광역시", "대전광역시", "서울특별시", "경상남도", "부산광역시", "전북특별자치도",
+            "울산광역시", "광주광역시", "강원특별자치도", "경상북도", "전라남도", "충청북도", "세종특별자치시"};
+
     public List<HotelResponseDto> getHotelInfo(String region) {
-        String[] regions = {"경기도", "제주특별자치도", "충청남도", "인천광역시", "대구광역시", "대전광역시", "서울특별시", "경상남도", "부산광역시", "전북특별자치도",
-                "울산광역시", "광주광역시", "강원특별자치도", "경상북도", "전라남도", "충청북도", "세종특별자치시"};
         if (Arrays.asList(regions).contains(region)) {
             List<HotelResponseDto> hotelInfos = hotelRepository.findByRegion(region).stream()
                     .map(Hotel::toDto).collect(Collectors.toList());
@@ -35,7 +36,6 @@ public class InfoProvideService {
     }
 
     public List<RestaurantResponseDto> getRestaurantInfo(String region) {
-        String[] regions = {"서울", "강원", "경기", "경남", "경북", "광주", "대구", "대전", "부산", "세종", "울산"};
         if (Arrays.asList(regions).contains(region)) {
             List<RestaurantResponseDto> restaurantInfos = restaurantRepository.findByRegion(region).stream()
                     .map(Restaurant::toDto).collect(Collectors.toList());
