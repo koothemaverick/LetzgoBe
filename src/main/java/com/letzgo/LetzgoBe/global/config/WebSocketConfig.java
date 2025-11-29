7736ed2bffca51023ad15d7b3eb5290e7959baec
구준휘 <121041290+KuJunhui@users.noreply.github.com>
2025-11-29 20:04:16 +0900

    Revert "feat: 이벤트 리스너에서 RDB 저장 후 웹소켓 브로드캐스트"

--- src/main/java/com/letzgo/LetzgoBe/global/config/WebSocketConfig.java
+++ src/main/java/com/letzgo/LetzgoBe/global/config/WebSocketConfig.java
@@ -1,9 +1,10 @@
-package com.letzgo.LetzgoBe.global.webSocket.config;
+package com.letzgo.LetzgoBe.global.config;
 
-import com.letzgo.LetzgoBe.global.webSocket.handler.ChatRoomWebSocketHandler;
-import com.letzgo.LetzgoBe.global.webSocket.handler.ChatWebSocketHandler;
+import com.letzgo.LetzgoBe.global.webSocket.ChatRoomWebSocketHandler;
+import com.letzgo.LetzgoBe.global.webSocket.ChatWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
--- src/main/java/com/letzgo/LetzgoBe/global/webSocket/ChatRoomWebSocketHandler.java
+++ src/main/java/com/letzgo/LetzgoBe/global/webSocket/ChatRoomWebSocketHandler.java
@@ -1,4 +1,4 @@
-package com.letzgo.LetzgoBe.global.webSocket.handler;
+package com.letzgo.LetzgoBe.global.webSocket;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
